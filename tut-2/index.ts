abad224573b5b7dd0ad527aeb69a2463a1cc7294
arshkhatwani <arsh.khatwani@gmail.com>
2024-04-02 11:59:15 +0530

    Add resolver functions

--- tut-2/index.ts
+++ tut-2/index.ts
@@ -1,9 +1,23 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from "./schema";
+import db from "./_db";
+
+const resolvers = {
+    Query: {
+        games() {
+            return db.games;
+        },
+        reviews: () => {
+            return db.reviews;
+        },
+        authors: () => db.authors,
+    },
+};
 
 const server = new ApolloServer({
     typeDefs,
+    resolvers,
 });
 
 const { url } = await startStandaloneServer(server, {
